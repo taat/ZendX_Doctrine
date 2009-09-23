4942d1a8b6b17b1b08d08f98007309340ddd8af1
Tomek Pęszor <tomek@taat.pl>
2009-09-23 13:31:54 +0200

    Clean up

--- scripts/doctrine-cli.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-IF EXIST "C:\program files\apache\php5\php.exe" GOTO :START
-:ERROR
-ECHO Error: wrong path to php.exe
-ECHO Edit doctrine-cli.bat to set up correct path
-GOTO :END
-:START
-"C:\program files\apache\php5\php.exe" doctrine-cli.sh %1
-:END
